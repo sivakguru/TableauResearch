1e20d78e85c23140f37aa61e48485528925f36ed
sivakguru <sivakguru@github.com>
2020-03-28 15:08:42 +0530

    Updating with commandline outputs

--- tableau_TSC_using_args.py
+++ tableau_TSC_using_args.py
@@ -18,6 +18,8 @@ import io
 import argparse
 #get password on the run
 import getpass
+#working with file directories
+import os
 
 def main():
     parser = argparse.ArgumentParser(description='Get all the Tableau Site details, to populate the React.js framework')
@@ -36,39 +38,89 @@ def main():
     #server sign-in
     server.auth.sign_in(tableau_auth)
 
-
+    #get all the pro, workbook, views and user Items from the site
     request_options = TSC.RequestOptions()
+    all_projects = list(TSC.Pager(server.projects, request_options))
     all_workbooks = list(TSC.Pager(server.workbooks, request_options))
     all_views = list(TSC.Pager(server.views, request_options))
     all_users = list(TSC.Pager(server.users, request_options))
+    #site name
+    site_id = server.sites.get_by_id(server.site_id).content_url
+    print('Collecting Details for Site : {}'.format(site_id))
 
-    #load all workbook items by using workbook ID
-    workbook_info = []
-    for wb in range(len(all_workbooks)):
-        workbook_info.append(server.workbooks.get_by_id(all_workbooks[wb].id))
+    #make the Current working directory as C:\Users\<user name\Document\Tableau
+    cwd = os.path.join(os.environ.get('HOMEDRIVE'),os.environ.get('HOMEPATH'),'Documents','Tableau')
+    #For each workbook, Create Folders for Preview images (.png) format
+    try:
+        os.mkdir(cwd)
+    except Exception as e:
+        print(e)
+        os.chdir(cwd)
+        print('\nCurrent working directory changed to : {}'.format(cwd))
+        print('\nAll the files and images will be stored in this location...!')
+    else:
+        os.chdir(cwd)
+        print('\nCurrent working directory changed to : {}'.format(cwd))
+        print('\nAll the files and images will be stored in this location...!')
+    finally:
+        print('''\nSaving all the workbook & related views thumbnail's in respective folders...!''')
+        for wb in range(len(all_workbooks)):
+            #projectname_workbookname
+            proj_wb_name = all_workbooks[wb].project_name+'_'+all_workbooks[wb].name
+            #views folder - thumbnail (.png) image of each workbook views
+            wb_view = all_workbooks[wb].name+'_views'
+            #create folder if the folder does not exist
+            if not os.path.exists(os.path.join(cwd, proj_wb_name)):
+                os.mkdir(os.path.join(cwd, proj_wb_name))
+            if not os.path.exists(os.path.join(cwd, proj_wb_name, wb_view)):
+                os.mkdir(os.path.join(cwd, proj_wb_name, wb_view))
+            server.workbooks.populate_preview_image(all_workbooks[wb])
+            server.workbooks.populate_views(all_workbooks[wb])
+            with open(os.path.join(cwd,proj_wb_name,'{}.png'.format(all_workbooks[wb].name)),'wb') as f_wb:
+                f_wb.write(all_workbooks[wb].preview_image)
+                f_wb.close()
+            for view in all_workbooks[wb].views:
+                server.views.populate_preview_image(view)
+                with open(os.path.join(cwd, proj_wb_name, wb_view,'{}.png'.format(view.name)), 'wb') as f_view:
+                    f_view.write(view.preview_image)
+                    f_view.close()
 
     #loop through Workbook Items and populate the data for Views under each workbook
     #each loop appends the data to the array
-
+    print('\nCollecting all the UI related data to a Data Frame')
     vw_id = []
     vw_name = []
     wb_id = []
     wb_name = []
+    wb_tags = []
+    wb_create_dt = []
+    wb_update_dt = []
+    show_tabs = []
     proj_id = []
     proj_name = []
+    is_default = []
+    proj_desc = []
     owner_id = []
     owner_name = []
     vw_image = []
     embed_code = []
 
-    for wb in range(len(workbook_info)): 
-        server.workbooks.populate_views(workbook_info[wb])
-        for view in workbook_info[wb].views:
-            wb_id.append(workbook_info[wb].id)
-            wb_name.append(workbook_info[wb].name)
-            proj_id.append(workbook_info[wb].project_id)
-            proj_name.append(workbook_info[wb].project_name)
-            owner_id.append(workbook_info[wb].owner_id)
+    for wb in range(len(all_workbooks)): 
+        server.workbooks.populate_views(all_workbooks[wb])
+        for view in all_workbooks[wb].views:
+            wb_id.append(all_workbooks[wb].id)
+            wb_name.append(all_workbooks[wb].name)
+            wb_tags.append(all_workbooks[wb].tags)
+            wb_create_dt.append(all_workbooks[wb].created_at)
+            wb_update_dt.append(all_workbooks[wb].updated_at)
+            show_tabs.append(all_workbooks[wb].show_tabs)
+            proj_id.append(all_workbooks[wb].project_id)
+            proj_name.append(all_workbooks[wb].project_name)
+            for proj in range(len(all_projects)):
+                if all_workbooks[wb].project_id == all_projects[proj].id:
+                    is_default.append(all_projects[proj].is_default())
+                    proj_desc.append(all_projects[proj].description)
+            owner_id.append(all_workbooks[wb].owner_id)
             for usr in range(len(all_users)):
                 if all_workbooks[wb].owner_id == all_users[usr].id:
                     owner_name.append(all_users[usr].name)
@@ -81,12 +133,12 @@ def main():
                     height='877' style='display:none;'>
                     <param name='host_url' value='https%3A%2F%2F10ax.online.tableau.com%2F' />
                     <param name='embed_code_version' value='3' />
-                    <param name='site_root' value='&#47;t&#47;vizsivadev749967' />
+                    <param name='site_root' value='&#47;t&#47;{}' />
                     <param name='name' value='{}&#47;{}' />
                     <param name='tabs' value='no' />
                     <param name='toolbar' value='yes' />
                     <param name='showAppBanner' value='false' /></object>
-            </div>""".format(workbook_info[wb].name.replace(' ', ''), view.name.replace(' ', '')))
+            </div>""".format(site_id, all_workbooks[wb].name.replace(' ', ''), view.name.replace(' ', '')))
 
 
     server.auth.sign_out()
@@ -99,8 +151,14 @@ def main():
     #load dictionary to data frame using Pandas
     df = pd.DataFrame.from_dict({'proj_id' : proj_id,
                                 'proj_name' : proj_name,
+                                'is_default' : is_default,
+                                'proj_desc' : proj_desc,
                                 'wb_id': wb_id,
                                 'wb_name' : wb_name,
+                                'wb_tags' : wb_tags,
+                                'wb_create_dt' : wb_create_dt,
+                                'wb_update_dt' : wb_update_dt,
+                                'show_tabs' : show_tabs,
                                 'vw_id' : vw_id,
                                 'vw_name' : vw_name,
                                 'owner_id' : owner_id,
@@ -108,8 +166,10 @@ def main():
                                 'vw_image' : vw_image,
                                 'embed_code' : embed_code})
 
-    df.to_csv(r'C:/Users/sivkumar/Documents/Project MOM/RR/Tableau/test.csv', index = False)
-
+    #file location inside the Current working directory with file name as 'site_<site_id>.csv'
+    file_location = os.path.join(cwd,'site_{}.csv'.format(site_id))
+    print('\nSaving data to csv in the location : {}'.format(file_location))
+    df.to_csv(r'{}'.format(file_location), index = False)
 
 if __name__ == "__main__":
     main()
--- tableau_TSC_worksop.py
+++ tableau_TSC_worksop.py
@@ -6,7 +6,20 @@
 
 
 import tableauserverclient as TSC
+#pandas for Data frame capabilities
 import pandas as pd
+#PIL for handeling Image files
+from PIL import Image
+#array
+from array import array
+#io to read bytes image data types
+import io
+#parse all the required arguments -- specific to a tableau server
+import argparse
+#get password on the run
+import getpass
+#working with file directories
+import os
 
 #set up authentication RR Deveoper account
 server = TSC.Server('https://10ax.online.tableau.com')
@@ -18,17 +31,20 @@ server.auth.sign_in(tableau_auth)
 
 #set up authentication RR Deveoper account
 server = TSC.Server('https://10ax.online.tableau.com')
+#if SSL Security fails use the uncomment the below line to make ssl certificate verification off
+# server.add_http_options({'verify': False})
 server.use_server_version()
-#tableau_auth = TSC.TableauAuth('thulasiramvanniya@gmail.com', 'Sairam@2903', 'thulasiramdev938591')
 tableau_auth = TSC.TableauAuth('siva.dhanush.007@gmail.com', 'Developer_123', 'vizsivadev749967')
 #sign-in to server
 server.auth.sign_in(tableau_auth)
 
+site_id = server.sites.get_by_id(server.site_id).content_url
 
 request_options = TSC.RequestOptions()
+all_projects = list(TSC.Pager(server.projects, request_options))
 all_workbooks = list(TSC.Pager(server.workbooks, request_options))
-# TSC.Pager(server.views, request_options)
 all_views = list(TSC.Pager(server.views, request_options))
+all_users = list(TSC.Pager(server.users, request_options))
 
 # #create a new project
 # with server.auth.sign_in(tableau_auth):
@@ -121,9 +137,18 @@ with server.auth.sign_in(tableau_auth):
     view_item = [view for view in all_views]   
 
 
-server.views.populate_preview_image(view_item)
-with open('C:/Users/sivkumar/Documents/Project MOM/RR/Tableau/view_preview_image.png') as f:
-	f.write(view_item.preview_image)
+for wb in range(len(workbook_info)): 
+    server.workbooks.populate_views(workbook_info[wb])
+    for view in workbook_info[wb].views:
+        server.views.populate_preview_image(view)
+        with open('C:/Users/sivkumar/Documents/Project MOM/RR/Tableau/{}.png'.format(view.name), 'wb') as f:
+            f.write(view.preview_image)
+
+
+for wb in range(len(workbook_info)):
+    server.workbooks.populate_preview_image(workbook_info[wb])
+    with open('C:/Users/sivkumar/Documents/Project MOM/RR/Tableau/{}.png'.format(workbook_info[wb].name),'wb') as f:
+        f.write(workbook_info[wb].preview_image)
 
 server.views.populate_image(view_item[0])
 server.views.populate_preview_image(view_item[0].image)
@@ -180,3 +205,41 @@ pic.show()
 #thumbnail of a workbook
 server.workbooks.populate_preview_image(workbook_info[0])
 wb_pic = Image.open(io.BytesIO(workbook_info[0].preview_image))
+
+python .\tableau_TSC_using_args.py -s 'https://10ax.online.tableau.com' -u 'siva.dhanush.007@gmail.com' -si 'vizsivadev749967'
+
+
+str(datetime.now().day)+'_'+str(datetime.now().month)+'_'+str(datetime.now().year)
+
+os.mkdir()
+os.chdir('C:/Users/sivkumar/Documents/Project MOM/RR/Tableau/')
+os.path.join
+
+#create the folder for each work book and populate with the work book images
+
+for wb in range(len(all_workbooks)):
+    os.mkdir(os.path.join(os.getcwd(),all_workbooks[wb].name))
+    server.workbooks.populate_preview_image(all_workbooks[wb])
+    with open(os.path.join(os.getcwd(),all_workbooks[wb].name,'{}.png'.format(all_workbooks[wb].name)),'wb') as f:
+        f.write(all_workbooks[wb].preview_image)
+
+
+try:
+    os.mkdir(os.path.join(os.environ.get('HOMEDRIVE'),os.environ.get('HOMEPATH'),'Documents','Tableau'))
+except Exception as e:
+    print(e)
+    os.chdir(os.path.join(os.environ.get('HOMEDRIVE'),os.environ.get('HOMEPATH'),'Documents','Tableau'))
+    print(os.getcwd())
+else:
+    os.chdir(os.path.join(os.environ.get('HOMEDRIVE'),os.environ.get('HOMEPATH'),'Documents','Tableau'))
+    print(os.getcwd())
+finally:
+    for wb in range(len(all_workbooks)):
+        os.mkdir(os.path.join(os.getcwd(),all_workbooks[wb].name))
+        server.workbooks.populate_preview_image(all_workbooks[wb])
+        with open(os.path.join(os.getcwd(),all_workbooks[wb].name,'{}.png'.format(all_workbooks[wb].name)),'wb') as f:
+            f.write(all_workbooks[wb].preview_image)
+            f.close()
+
+os.rmdir()
+os.
